8cb458ad3a424fb9f831ffc9622e473f2ecfe1ba
nasirusmanabdullahi123 <nasirusmanabdullahi123@gmial.com>
2026-08-27 20:21:45 -0700

    modified changes

--- start.py
+++ start.py
@@ -1,3 +1,9 @@
-print("welcome")
+"""
+Create a sorting algorithm that sorts a list of numbers in ascending order 
+using the bubble sort technique. The algorithm should iterate through the list multiple times, comparing adjacent elements and swapping them if they are in the wrong order. 
+The process should continue until no more swaps are needed, indicating that the list is sorted.
 
-print("new add")
+"""
+
+
+#[1,4,5,6,8,2,3,7] ===> [1,2,3,4,5,6,7,8]
