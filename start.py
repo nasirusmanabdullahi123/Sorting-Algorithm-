6e454417f38576f89be978d3de6814f06e024871
nasirusmanabdullahi123 <nasirusmanabdullahi123@gmial.com>
2026-08-28 03:51:57 -0700

    to be cotinued

--- start.py
+++ start.py
@@ -4,6 +4,14 @@ using the bubble sort technique. The algorithm should iterate through the list m
 The process should continue until no more swaps are needed, indicating that the list is sorted.
 
 """
-
-
 #[1,4,5,6,8,2,3,7] ===> [1,2,3,4,5,6,7,8]
+
+def sorting(num):
+    for i in range(len(num)):           
+        for j in range(0, len(num)-i-1):
+            if num[j] > num[j+1]:
+                num[j], num[j+1] = num[j+1], num[j]
+
+num = [1,4,5,6,8,2,3,7]
+num.sort(num)
+print(num)
